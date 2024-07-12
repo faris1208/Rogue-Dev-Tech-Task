bd1d95236a32324d859d6f9793b37546fc96682e
faris1208 <farislawal3@gmail.com>
2024-07-12 11:14:29 -0700

    changes

--- src/app/components/hero/index.jsx
+++ src/app/components/hero/index.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <div className={styles.hero_container}>
         <NavBar />
         <div className={styles.rogue_image}>
-        <section>
+        <section className={styles.Unveil_wrapper}>
             <h5>Let’s help you</h5>
             <h2>Unveil <span>efficiency,</span> elevate <span>productivity.</span> Experience Rogue’s design today</h2>
             <p>Leading digital agency with solid design and development expertise</p>
